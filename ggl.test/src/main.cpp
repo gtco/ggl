3239129da192d008f66c43cbb62a039c210fab95
Greg Copeland <greg.copeland@me.com>
2016-08-18 14:17:11 -0400

    Added simplified math functions

--- ggl.test/src/main.cpp
+++ ggl.test/src/main.cpp
@@ -1,87 +1,91 @@
 #include <stdio.h>
 
 #include "camera2d.h"
-#include "linear_algebra.h"
+#include "math2d.h"
 #include "kazmath.h"
 
 #define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+// All 16 valus in a 4x4 matrix must match
+#define GOAL	16
 
-int compare(glm::mat4 gl4, kmMat4 km4);
-int compare_la(Matrix gl4, kmMat4 km4);
-void test_km();
-void test_la();
+int compare_glm(glm::mat4 gl4, kmMat4 km4);
+int compare_la(struct ggl_mat4 gl4, kmMat4 km4);
+void test_km(int screen_width, int screen_height, glm::vec2 position);
+void test_la(int screen_width, int screen_height, glm::vec2 position);
 
 int main(int argc, char** argv)
 {
-	test_km();
-	test_la();
+	int screen_width = 640;
+	int screen_height = 480;
+	glm::vec2 position(0.0f, 0.0f);
+	float scale = 1.0f;
+
+	test_km(screen_width, screen_height, position);
+	test_la(screen_width, screen_height, position);
+
 	getchar();
 }
 
-void test_la()
+void test_la(int screen_width, int screen_height, glm::vec2 position)
 {
-	int screen_width = 640;
-	int screen_height = 480;
-	glm::vec2 position(0.0f, 0.0f);
+	struct ggl_mat4 ortho = ggl_orthogonal(0.0f, (float)screen_width, 0.0f, (float)screen_height);
+	struct ggl_mat4 camera = IDENTITY_MATRIX;
+	struct ggl_mat4 tmp = IDENTITY_MATRIX;
+	ggl_translate(&tmp, -position.x + (screen_width / 2), -position.y + (screen_height / 2), 0.0f);
+	ggl_multiplymat4(&camera, &ortho, &tmp);
+	tmp = IDENTITY_MATRIX;
+	ggl_scale(&tmp, 1.0f, 1.0f, 0.0f);
+	ggl_multiplymat4(&camera, &camera, &tmp);
 
-	kmMat4 kom;
-	kmMat4 tmp;
-	kmMat4 com;
+	kmMat4 kmOrtho;
+	kmMat4 kmCamera;
+	kmMat4 kmTmp;
+	kmMat4OrthographicProjection2(&kmOrtho, 0.0f, (float)screen_width, 0.0f, (float)screen_height);
+	kmMat4Identity(&kmCamera);
+	kmMat4Translation(&kmTmp, -position.x + (screen_width / 2), -position.y + (screen_height / 2), 0.0f);
+	kmMat4Multiply(&kmCamera, &kmOrtho, &kmTmp);
+	kmMat4Scaling(&kmTmp, 1.0f, 1.0f, 0.0f);
+	kmMat4Multiply(&kmCamera, &kmCamera, &kmTmp);
 
-	kmMat4OrthographicProjection2(&kom, 0.0f, (float)screen_width, 0.0f, (float)screen_height);
-	kmMat4Identity(&com);
-	kmMat4Translation(&tmp, -position.x + (screen_width / 2), -position.y + (screen_height / 2), 0.0f);
-	kmMat4Multiply(&com, &kom, &tmp);
-	kmMat4Scaling(&tmp, 1.0f, 1.0f, 0.0f);
-	kmMat4Multiply(&com, &com, &tmp);
-
-	Matrix ortho = orthogonal(0.0f, (float)screen_width, 0.0f, (float)screen_height);
-	Matrix mtmp = IDENTITY_MATRIX;
-	Matrix camera = IDENTITY_MATRIX;
-	translate(&mtmp, -position.x + (screen_width / 2), -position.y + (screen_height / 2), 0.0f);
-	multiplymat4_ex(&camera, &ortho, &mtmp);
-	mtmp = IDENTITY_MATRIX;
-	scale(&mtmp, 1.0f, 1.0f, 0.0f);
-	multiplymat4_ex(&camera, &camera, &mtmp);
-	
-	int match_ortho = compare_la(ortho, kom);
-	int match_camera = compare_la(camera, com);
-	bool b = (match_ortho == 16) && (match_camera == 16);
+	bool b = (compare_la(ortho, kmOrtho) == GOAL) && (compare_la(camera, kmCamera) == GOAL);
 	debug("calculations for la and kazmath match : %s", b ? "true" : "false");
 }
 
-void test_km()
+void test_km(int screen_width, int screen_height, glm::vec2 position)
 {
 	Camera2d c;
-
-	int screen_width = 640;
-	int screen_height = 480;
-
 	c.init(screen_height, screen_width);
-	glm::vec2 position(0.0f, 0.0f);
-
-	Matrix om = orthogonal(0.0f, (float)screen_width, 0.0f, (float)screen_height);
-	kmMat4 kom;
-	kmMat4OrthographicProjection2(&kom, 0.0f, (float)screen_width, 0.0f, (float)screen_height);
-
 	c.set_position(position);
 	c.update();
 
-	kmMat4 tmp;
-	kmMat4 com;
-	kmMat4Identity(&com);
-	kmMat4Translation(&tmp, -position.x + (screen_width / 2), -position.y + (screen_height / 2), 0.0f);
-	kmMat4Multiply(&com, &kom, &tmp);
-	kmMat4Scaling(&tmp, 1.0f, 1.0f, 0.0f);
-	kmMat4Multiply(&com, &com, &tmp);
+	kmMat4 kmOrtho;
+	kmMat4 kmCamera;
+	kmMat4 kmTmp;
+	kmMat4OrthographicProjection2(&kmOrtho, 0.0f, (float)screen_width, 0.0f, (float)screen_height);
+	kmMat4Identity(&kmCamera);
+	kmMat4Translation(&kmTmp, -position.x + (screen_width / 2), -position.y + (screen_height / 2), 0.0f);
+	kmMat4Multiply(&kmCamera, &kmOrtho, &kmTmp);
+	kmMat4Scaling(&kmTmp, 1.0f, 1.0f, 0.0f);
+	kmMat4Multiply(&kmCamera, &kmCamera, &kmTmp);
 
-	int match_ortho = compare(c.get_ortho_matrix(), kom);
-	int match_camera = compare(c.get_camera_matrix(), com);
-	bool b = (match_ortho == 16) && (match_camera == 16);
+	// Move
+
+	glm::vec2 new_position = glm::vec2(1.0f, -1.0f);
+	c.set_position(new_position);
+//	c.set_scale(1.5f);
+	c.update();
+
+	kmMat4Translation(&kmTmp, -new_position.x + (screen_width / 2), -new_position.y + (screen_height / 2), 0.0f);
+	kmMat4Multiply(&kmCamera, &kmOrtho, &kmTmp);
+//	kmMat4Scaling(&kmTmp, 1.5f, 1.5f, 0.0f);
+//	kmMat4Multiply(&kmCamera, &kmCamera, &kmTmp);
+
+
+	bool b = (compare_glm(c.get_ortho_matrix(), kmOrtho) == GOAL) && (compare_glm(c.get_camera_matrix(), kmCamera) == GOAL);
 	debug("calculations for glm and kazmath match : %s", b ? "true" : "false");
 }
 
-int compare(glm::mat4 gl4, kmMat4 km4)
+int compare_glm(glm::mat4 gl4, kmMat4 km4)
 {
 	int matches = 0;
 	int c = 0;
@@ -104,7 +108,7 @@ int compare(glm::mat4 gl4, kmMat4 km4)
 	return matches;
 }
 
-int compare_la(Matrix gl4, kmMat4 km4)
+int compare_la(struct ggl_mat4 gl4, kmMat4 km4)
 {
 	int matches = 0;
 	int c = 0;
---
+++ ggl.test/src/math2d.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <math.h>
+
+struct ggl_mat4 {
+	float m[16];
+};
+
+union ggl_vec4 {
+	float m[4];
+	struct {
+		float x, y, z, w;
+	};
+};
+
+static const struct ggl_mat4 IDENTITY_MATRIX = {{ 
+	1, 0, 0, 0,
+	0, 1, 0, 0,
+	0, 0, 1, 0,
+	0, 0, 0, 1
+}};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	void ggl_identity(struct ggl_mat4* mat4);
+	void ggl_rotateX(const struct ggl_mat4* m, float angle);
+	void ggl_rotateY(const struct ggl_mat4* m, float angle);
+	void ggl_scale(const struct ggl_mat4* m, float x, float y, float z);
+	void ggl_translate(const struct ggl_mat4* m, float x, float y, float z);
+	struct ggl_mat4 ggl_orthogonal(float left, float right, float bottom, float top);
+	void ggl_multiplymat4(struct ggl_mat4* result, const struct ggl_mat4* m1, const struct ggl_mat4* m2);
+
+#ifdef __cplusplus
+}
+#endif
+
+
